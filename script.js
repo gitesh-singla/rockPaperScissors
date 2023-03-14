7e4c806dfb62425c446da06030219a7b66cfe28b
gitesh <124420820+gitesh-singla@users.noreply.github.com>
2023-03-14 18:59:19 +0530

    Test function for playin single round added

---
+++ script.js
@@ -0,0 +1,28 @@
+let playerChoice , computerChoice;
+let plays = ["rock" , "paper" , "scissors"];
+
+function getComputerChoice(){
+    return Math.floor(Math.random() * plays.length);
+}
+
+playerChoice = prompt("Enter your choice :");
+computerChoice = getComputerChoice();
+
+function playRound (playerChoice , computerChoice){
+    playerChoice = playerChoice.toUpperCase();
+    computerChoice  = computerChoice.toUpperCase();
+
+    if(playerChoice == computerChoice){
+        console.log("Round Tied!");
+    }
+    else if(playerChoice == "ROCK"){
+        computerChoice == "SCISSORS" ? console.log("Round Win!") : console.log("Round Lost!");
+    }
+    else if(playerChoice == "PAPER"){
+        computerChoice == "ROCK" ? console.log("Round Win!") : console.log("Round Lost!");
+    }
+    else {
+        computerChoice == "PAPER" ? console.log("Round Win!") : console.log("Round Lost!");
+    }
+}
+
